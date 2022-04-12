d902418033ae4971b01250971307b1e70cb83a5f
Jess Phillips <tripphillips5@gmail.com>
2022-04-12 13:38:20 -0500

    create FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Trip Phillips on 4/12/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUrl: URL
+}
