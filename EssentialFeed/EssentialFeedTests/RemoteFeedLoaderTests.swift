5f2d50bda3cb1cfd41b605a9458b310cc8fcceaf
Jess Phillips <tripphillips5@gmail.com>
2022-04-18 22:48:08 -0500

    refactor(RemoteFeedLoaderTests): give `RemoteFeedLoader` a url via constructor injection

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,21 +8,21 @@
 import XCTest
 
 class RemoteFeedLoader {
+    let url: URL
     let client: HTTPClient
     
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
+        self.url = url
         self.client = client
     }
     
     func load() {
-        client.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: url)
     }
 }
 
-
-
 protocol HTTPClient {
-    func get(from url: URL) 
+    func get(from url: URL)
 }
 
 class HTTPClientSpy: HTTPClient {
@@ -37,18 +37,20 @@ class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        let _ = RemoteFeedLoader(client: client)
+        let url = URL(string: "https://a-given-url.com")!
+        let _ = RemoteFeedLoader(url: url, client: client)
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let url = URL(string: "https://a-given-url.com")!
+        let sut = RemoteFeedLoader(url: url, client: client)
         
         sut.load()
         
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(url, client.requestedURL)
     }
 
 }
