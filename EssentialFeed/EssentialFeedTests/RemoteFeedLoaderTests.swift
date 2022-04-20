d000a66cc7eb5b5ed00e649dc2acb8e899589922
Jess Phillips <tripphillips5@gmail.com>
2022-04-20 17:54:32 -0500

    test(RemoteFeedLoaderTests): guarantee that we can call `load` method in `RemoteFeedLoader` more than once.

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -25,6 +25,16 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(url, client.requestedURL)
     }
     
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
     
     // MARK: - Helpers
     
@@ -36,9 +46,11 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
+        var requestedURLs = [URL]()
         
         func get(from url: URL) {
             requestedURL = url
+            requestedURLs.append(url)
         }
     }
 }
