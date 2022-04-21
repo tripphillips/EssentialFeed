89467e042cae488417568c8fae9c6fddf18d7d23
Jess Phillips <tripphillips5@gmail.com>
2022-04-21 13:50:58 -0500

    task(RemoteFeedLoader): move Decodable logic to a new private item struct to decouple the Feed Feature Modular from API Implementation details

--- EssentialFeed/EssentialFeed/FeedItem.swift
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -23,12 +23,3 @@ public struct FeedItem: Equatable {
         self.imageURL = imageURL
     }
 }
-
-extension FeedItem: Decodable {
-    enum CodingKeys: String, CodingKey {
-        case id
-        case description
-        case location
-        case imageURL = "image"
-    }
-}
