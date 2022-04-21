7d188f84ddc4707f407e0788e0393a6bd32e91d1
Jess Phillips <tripphillips5@gmail.com>
2022-04-21 12:51:05 -0500

    test(RemoteFeedLoaderTests): handle data in tests if status code is 200

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -51,7 +51,8 @@ class RemoteFeedLoaderTests: XCTestCase {
         
         sampleCodes.enumerated().forEach { index, code in
             expect(sut, toCompleteWith: .failure(.invalidData)) {
-                client.complete(withStatusCode: 200, at: index)
+                let json = makeItemsJSON([])
+                client.complete(withStatusCode: code, data: json, at: index)
             }
         }
     }
@@ -156,7 +157,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         }
         
         func complete(withStatusCode code: Int,
-                      data: Data = Data(),
+                      data: Data,
                       at index: Int = 0) {
             let response = HTTPURLResponse(url: requestedURLs[index],
                                            statusCode: code,
