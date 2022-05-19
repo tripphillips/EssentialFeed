d1c39a8b3616b1d5278b6e4c3d42cb24be64f634
Jess Phillips <tripphillips5@gmail.com>
2022-05-19 10:51:25 -0500

    task(LocalFeedLoader): load completes retrieval with no images on empty cache

--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -55,4 +55,8 @@ class FeedStoreSpy: FeedStore {
     func completeRetreival(with error: Error, at index: Int = 0) {
         retreivalCompletions[index](error)
     }
+    
+    func completeRetrievalWithEmptyCache(at index: Int = 0) {
+        retreivalCompletions[index](nil)
+    }
 }
