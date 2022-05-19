9d63c443256f151c669627f35d46df69141789db
Jess Phillips <tripphillips5@gmail.com>
2022-05-18 22:58:48 -0500

    task(LocalFeedItem): move to separate file

---
+++ EssentialFeed/EssentialFeed/LocalFeedItem.swift
@@ -0,0 +1,25 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Trip Phillips on 5/18/22.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID,
+                description: String?,
+                location: String?,
+                imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
