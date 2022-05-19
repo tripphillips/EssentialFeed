62b7076cf70f439a4522eca6219b5218e9dc0cd9
Jess Phillips <tripphillips5@gmail.com>
2022-05-19 17:58:17 -0500

    task(LocalFeedLoader): load does not deliver results after instance had been deallocated

--- EssentialFeed/EssentialFeed/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/LocalFeedLoader.swift
@@ -33,8 +33,10 @@ public final class LocalFeedLoader {
         }
     }
     
-    public func load(completion: @escaping (LoadResult?) -> ()) {
-        store.retrieve { [unowned self] result in
+    public func load(completion: @escaping (LoadResult) -> ()) {
+        store.retrieve { [weak self] result in
+            guard let self = self else { return }
+            
             switch result {
             case let .failure(error):
                 self.store.deleteCacheFeed { _ in }
