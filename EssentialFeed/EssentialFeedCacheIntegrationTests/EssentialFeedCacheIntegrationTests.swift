844ead463766ae954c6cdd603d5b4890c83de202
Jess Phillips <tripphillips5@gmail.com>
2022-07-01 08:20:25 -0500

    task(EssentialFeed): add `EssentialFeedCacheIntegrationTests` target - separate potentially slower cache integration tests from fast unit/isolated tests

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,21 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Trip Phillips on 7/1/22.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+   
+}
