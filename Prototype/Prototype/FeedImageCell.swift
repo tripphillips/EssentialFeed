210f0495bcbd7c45e55b070eea30673d3c086d77
Jess Phillips <tripphillips5@gmail.com>
2022-09-21 17:55:05 -0500

    task(FeedImageCell): render simulated feed data in the app

---
+++ Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  Prototype
+//
+//  Created by Trip Phillips on 9/21/22.
+//
+
+import UIKit
+
+final class FeedImageCell: UITableViewCell {
+    @IBOutlet private(set) var descriptionLabel: UILabel!
+    @IBOutlet private(set) var feedImageView: UIImageView!
+    @IBOutlet private(set) var locationLabel: UILabel!
+    @IBOutlet private(set) var locationContainer: UIView!
+}
