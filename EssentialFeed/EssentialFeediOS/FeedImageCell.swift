317ebef5080f8d940407dd5883dff32eb5348d8f
Jess Phillips <tripphillips5@gmail.com>
2022-11-12 13:37:12 -0700

    task(FeedViewController); render loaded feed on successful completion - add FeedImageCell - add loadFeedCompletion tests - add FeedViewController tableView methods for loading cells

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Trip Phillips on 11/12/22.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
