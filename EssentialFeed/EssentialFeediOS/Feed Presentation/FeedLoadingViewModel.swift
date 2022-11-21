4f20a87f7bf7e5ebbec6624d468642a096dc0a36
Jess Phillips <tripphillips5@gmail.com>
2022-11-20 17:31:26 -0700

    task(EssentialFeediOS): convert `FeedImageViewModel` logic to `FeedImagePresenter` and move View Models to separate files.

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Trip Phillips on 11/20/22.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Trip Phillips on 11/20/22.
+//
+
+import Foundation
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
