5dae0e0a38fd474478e14782ba23277036d5488a
Jess Phillips <tripphillips5@gmail.com>
2022-11-28 08:25:32 -0600

    task(FeedViewControllersTests): add helper method for string localization

---
+++ EssentialFeed/EssentialFeediOSTests/Helpers/FeedViewController+Localization.swift
@@ -0,0 +1,22 @@
+//
+//  FeedViewController+Localization.swift
+//  EssentialFeediOSTests
+//
+//  Created by Trip Phillips on 11/28/22.
+//
+
+import Foundation
+import XCTest
+import EssentialFeediOS
+
+extension FeedViewControllerTests {
+    func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+        let table = "Feed"
+        let bundle = Bundle(for: FeedViewController.self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
+        }
+        return value
+    }
+}
