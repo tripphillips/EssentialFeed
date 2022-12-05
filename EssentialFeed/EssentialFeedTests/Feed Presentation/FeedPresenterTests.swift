9f48bcae9c6dddbda3f3c59b565f9ae508e730ac
Jess Phillips <tripphillips5@gmail.com>
2022-12-05 09:10:09 -0700

    task(EssentialFeedTests): add FeedPresenter tests to move FeedPresenter logic to EssentialFeed module

---
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -0,0 +1,14 @@
+//
+//  FeedPresenterTests.swift
+//  EssentialFeedTests
+//
+//  Created by Trip Phillips on 12/5/22.
+//
+
+import XCTest
+
+class FeedPresentationTests: XCTestCase {
+    
+    
+    
+}
