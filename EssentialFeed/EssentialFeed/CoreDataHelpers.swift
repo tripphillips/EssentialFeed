38009b5bfcf9d7ed8cc34572b9a80765d2a146ad
Jess Phillips <tripphillips5@gmail.com>
2022-12-11 10:07:48 -0700

    task(EssentialFeed): update local feed image data storage

--- EssentialFeed/EssentialFeed/CoreDataHelpers.swift
+++ EssentialFeed/EssentialFeed/CoreDataHelpers.swift
@@ -13,8 +13,7 @@ extension NSPersistentContainer {
         case failedToLoadPersistentStores(Swift.Error)
     }
     
-    static func load(modelName name: String, url: URL, in bundle: Bundle) throws -> NSPersistentContainer {
-        guard let model = NSManagedObjectModel.with(name: name, in: bundle) else { throw LoadingError.modelNotFound }
+    static func load(name: String, model: NSManagedObjectModel, url: URL) throws -> NSPersistentContainer {
         
         let description = NSPersistentStoreDescription(url: url)
         let container = NSPersistentContainer(name: name, managedObjectModel: model)
@@ -22,13 +21,13 @@ extension NSPersistentContainer {
         
         var loadError: Swift.Error?
         container.loadPersistentStores { _, error in loadError = error }
-        try loadError.map { error in throw LoadingError.failedToLoadPersistentStores(error) }
+        try loadError.map { throw $0 }
         
         return container
     }
 }
 
-private extension NSManagedObjectModel {
+extension NSManagedObjectModel {
     static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
         return bundle
             .url(forResource: name, withExtension: "momd")
