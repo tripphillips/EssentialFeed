204f70cbcca4c041c7b1c4e91f5587db402d4ab2
Jess Phillips <tripphillips5@gmail.com>
2022-12-11 17:44:02 -0700

    task(FeedLoaderWIthFallbackComposite): move `FeedLoaderWithFallbackComposite` to production

--- EssentialFeed/EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialFeed/EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -1,5 +1,5 @@
 //
-//  RemoteWithLocalFallbackCompositeTests.swift
+//  FeedLoaderWithFallbackCompositeTests.swift
 //  EssentialAppTests
 //
 //  Created by Trip Phillips on 12/11/22.
@@ -7,29 +7,9 @@
 
 import XCTest
 import EssentialFeed
+import EssentialApp
 
-class RemoteWithLocalFallbackComposite: FeedLoader {
-    private let primary: FeedLoader
-    private let fallback: FeedLoader
-    
-    init(primary: FeedLoader, fallback: FeedLoader) {
-        self.primary = primary
-        self.fallback = fallback
-    }
-    
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        primary.load { [weak self] result in
-            switch result {
-            case .success:
-                completion(result)
-            case .failure:
-                self?.fallback.load(completion: completion)
-            }
-        }
-    }
-}
-
-final class RemoteWithLocalFallbackCompositeTests: XCTestCase {
+final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
     
     func test_load_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
         let primaryFeed = uniqueFeed()
@@ -57,7 +37,7 @@ final class RemoteWithLocalFallbackCompositeTests: XCTestCase {
     private func makeSUT(primaryResult: FeedLoader.Result, fallbackResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) -> FeedLoader {
         let primaryLoader = LoaderStub(result: primaryResult)
         let fallbackLoader = LoaderStub(result: fallbackResult)
-        let sut = RemoteWithLocalFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
         trackForMemoryLeaks(primaryLoader, file: file, line: line)
         trackForMemoryLeaks(fallbackLoader, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
