3d8261ca3112bc141e5f8e8e1cd29bc070c6e367
Jess Phillips <tripphillips5@gmail.com>
2022-12-20 08:37:44 -0700

    task(EssentialApp): remove unused debugging code that was previously used during UI tests

--- EssentialFeed/EssentialApp/EssentialApp/AppDelegate.swift
+++ EssentialFeed/EssentialApp/EssentialApp/AppDelegate.swift
@@ -8,17 +8,4 @@
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        let configuration = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-        
-        #if DEBUG
-        configuration.delegateClass = DebuggingSceneDelegate.self
-        #endif
-        
-        return configuration
-    }
-}
+class AppDelegate: UIResponder, UIApplicationDelegate {}
