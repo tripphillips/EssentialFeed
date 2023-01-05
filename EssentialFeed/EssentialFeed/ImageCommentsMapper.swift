f9feec11111c716cf07dbd237b4c22916435ed2d
Jess Phillips <tripphillips5@gmail.com>
2023-01-05 07:24:52 -0700

    Duplicate `RemoteFeedLoader` as `RemoteImageCommentsLoader`

---
+++ EssentialFeed/EssentialFeed/ImageCommentsMapper.swift
@@ -0,0 +1,24 @@
+//
+//  ImageCommentsMapper.swift
+//  EssentialFeed
+//
+//  Created by Trip Phillips on 1/5/23.
+//
+
+import Foundation
+
+final class ImageCommentsMapper {
+    
+    private struct Root: Decodable {
+        let items: [RemoteFeedItem]
+    }
+    
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+        guard response.isOK,
+              let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            throw RemoteImageCommentsLoader.Error.invalidData
+        }
+        
+        return root.items
+    }
+}
