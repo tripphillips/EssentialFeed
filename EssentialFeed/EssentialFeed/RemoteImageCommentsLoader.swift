0cbee220b2f4fcfaab9f47655cef68f85905257c
Jess Phillips <tripphillips5@gmail.com>
2023-01-06 21:07:17 -0700

    task(RemoteImageCommentsLoader): remove duplicate private extension

--- EssentialFeed/EssentialFeed/RemoteImageCommentsLoader.swift
+++ EssentialFeed/EssentialFeed/RemoteImageCommentsLoader.swift
@@ -45,14 +45,3 @@ public final class RemoteImageCommentsLoader {
         }
     }
 }
-
-private extension Array where Element == RemoteFeedItem {
-    func toModels() -> [FeedImage] {
-        return map {
-            FeedImage(id: $0.id,
-                     description: $0.description,
-                     location: $0.location,
-                     url: $0.image)
-        }
-    }
-}
