938d3753b682acb8a520eb182d4541d4898adb04
Jess Phillips <tripphillips5@gmail.com>
2023-01-07 21:20:07 -0700

    task(EssentialFeed): move "GENERIC_CONNECTION_ERROR" localization key to new Shared.strings

---
+++ EssentialFeed/EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+Localization.swift
@@ -0,0 +1,24 @@
+//
+//  FeedUIIntegrationTests+Localization.swift
+//  EssentialFeediOSTests
+//
+//  Created by Trip Phillips on 11/28/22.
+//
+
+import Foundation
+import XCTest
+import EssentialFeed
+
+extension FeedUIIntegrationTests {
+    private class DummyView: ResourceView {
+        func display(_ viewModel: Any) {}
+    }
+    
+    var loadError: String {
+        LoadResourcePresenter<Any, DummyView>.loadError
+    }
+    
+    var feedTitle: String {
+        FeedPresenter.title
+    }
+}
