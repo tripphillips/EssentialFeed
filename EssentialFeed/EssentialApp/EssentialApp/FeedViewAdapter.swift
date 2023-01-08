6cd8206fded55c5236c25659cd2be74c58778c67
Jess Phillips <tripphillips5@gmail.com>
2023-01-08 15:25:38 -0700

    task(EssentialApp): remove unused FeedImagePresentation logic

--- EssentialFeed/EssentialApp/EssentialApp/FeedViewAdapter.swift
+++ EssentialFeed/EssentialApp/EssentialApp/FeedViewAdapter.swift
@@ -25,7 +25,7 @@ final class FeedViewAdapter: ResourceView {
                                                               imageLoader(model.url)
                                                           })
             let view = FeedImageCellController(
-                viewModel: FeedImagePresenter<FeedImageCellController, UIImage>.map(model),
+                viewModel: FeedImagePresenter.map(model),
                 delegate: adapter)
             
             adapter.presenter = LoadResourcePresenter(
