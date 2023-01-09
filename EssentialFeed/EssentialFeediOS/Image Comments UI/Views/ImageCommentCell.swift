8df6557edb7674f63d83e4cdef5669368e216dcb
Jess Phillips <tripphillips5@gmail.com>
2023-01-08 23:39:39 -0700

    task(EssentialFeed): implement Image Comments UI

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Trip Phillips on 1/8/23.
+//
+
+import UIKit
+
+final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+}
---
+++ EssentialFeed/EssentialFeediOSTests/Image Comments UI/ImageCommentsSnapshotTests.swift
@@ -0,0 +1,47 @@
+//
+//  ImageCommentsSnapshotTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Trip Phillips on 1/8/23.
+//
+
+import XCTest
+import EssentialFeediOS
+@testable import EssentialFeed
+
+class ImageCommentsSnapshotTests: XCTestCase {
+    
+    func test_feedWithContent() {
+        let sut = makeSUT()
+        
+        sut.display(comments())
+        
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "IMAGE_COMMENTS_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "IMAGE_COMMENTS_dark")
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> ListViewController {
+        let bundle = Bundle(for: ListViewController.self)
+        let storyboard = UIStoryboard(name: "ImageComments", bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! ListViewController
+        controller.loadViewIfNeeded()
+        controller.tableView.showsVerticalScrollIndicator = false
+        controller.tableView.showsHorizontalScrollIndicator = false
+        return controller
+    }
+
+    private func comments() -> [CellController] {
+        return [
+            ImageCommentCellController(
+                model: ImageCommentViewModel(message: "Today I wrote my first SwiftUI view.  It was really cool to see my code instantly compile and display in the preview.", date: "3 years ago", username: "HelloUser")),
+            ImageCommentCellController(
+                model: ImageCommentViewModel(message: "This is a small comment", date: "10 days ago", username: "JohnSmith")
+            ),
+            ImageCommentCellController(
+                model: ImageCommentViewModel(message: "nice", date: "1 hour ago", username: "Thisisaverylongusernamefortesting")
+            )
+        ]
+    }
+}
