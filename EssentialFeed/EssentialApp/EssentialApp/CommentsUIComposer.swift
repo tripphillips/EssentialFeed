b10aaf39f19f377b2772e25535ff5d602993d9f7
Jess Phillips <tripphillips5@gmail.com>
2023-01-14 22:11:45 -0700

    task(EssentialApp): duplicate FeedUIComposer as CommentsUIComposer

---
+++ EssentialFeed/EssentialApp/EssentialApp/CommentsUIComposer.swift
@@ -0,0 +1,42 @@
+//
+//  CommentsUIComposer.swift
+//  EssentialApp
+//
+//  Created by Trip Phillips on 1/14/23.
+//
+
+import UIKit
+import Combine
+import EssentialFeed
+import EssentialFeediOS
+
+public final class CommentsUIComposer {
+    private init() {}
+    
+    private typealias FeedPresentationAdapter = LoadResourcePresentationAdapter<[FeedImage], FeedViewAdapter>
+    
+    static func commentsComposedWith(
+        commentsLoader: @escaping () -> AnyPublisher<[FeedImage], Error>    ) -> ListViewController {
+        let presentationAdapter = FeedPresentationAdapter(loader: commentsLoader)
+  
+        let feedController = makeFeedViewController(title: FeedPresenter.title)
+        feedController.onRefresh = presentationAdapter.loadResource
+        
+        presentationAdapter.presenter = LoadResourcePresenter(
+            resourceView: FeedViewAdapter(controller: feedController,
+                                          imageLoader: { _ in Empty<Data, Error>().eraseToAnyPublisher()} ),
+            loadingView: WeakRefVirtualProxy(feedController),
+            errorView: WeakRefVirtualProxy(feedController),
+            mapper: FeedPresenter.map)
+        
+        return feedController
+    }
+    
+    private static func makeFeedViewController(title: String) -> ListViewController {
+        let bundle = Bundle(for: ListViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let feedController = storyboard.instantiateInitialViewController() as! ListViewController
+        feedController.title = FeedPresenter.title
+        return feedController
+    }
+}
--- EssentialFeed/EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
+++ EssentialFeed/EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
@@ -11,7 +11,7 @@ import EssentialFeed
 import EssentialFeediOS
 @testable import EssentialApp
 
-final class FeedUIIntegrationTests: XCTestCase {
+class FeedUIIntegrationTests: XCTestCase {
     
     func test_feedView_hasTitle() {
         let (sut, _) = makeSUT()
