12a99246e85ff230cfa6aee32a32e40861b25485
Jess Phillips <tripphillips5@gmail.com>
2023-01-14 23:50:41 -0700

    task(CommentsUIIntegrationTests); proves request is canceled on comments view deinit

--- EssentialFeed/EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
+++ EssentialFeed/EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
@@ -134,6 +134,29 @@ class CommentsUIIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
    
+    func test_deinit_cancelsRunningRequest() {
+        var cancelCallCount = 0
+        
+        var sut: ListViewController?
+        
+        autoreleasepool {
+            sut = CommentsUIComposer.commentsComposedWith {
+                PassthroughSubject<[ImageComment], Error>()
+                    .handleEvents(receiveCancel: {
+                        cancelCallCount += 1
+                    }).eraseToAnyPublisher()
+            }
+            
+            sut?.loadViewIfNeeded()
+        }
+        
+        XCTAssertEqual(cancelCallCount, 0)
+        
+        sut = nil
+        
+        XCTAssertEqual(cancelCallCount, 1)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: ListViewController, loader: LoaderSpy) {
