095240882fd63b102853988f9d3c06a65324270c
Jess Phillips <tripphillips5@gmail.com>
2023-01-31 23:26:01 -0700

    task(FeedStore): remove unused async methods

--- EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FeedStoreSpecs.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FeedStoreSpecs.swift
@@ -116,7 +116,7 @@ extension FeedStoreSpecs where Self: XCTestCase {
     }
     
     func expect(_ sut: FeedStore,
-                toRetrieveTwice expectedResult: FeedStore.RetrievalResult,
+                toRetrieveTwice expectedResult: Result<CachedFeed?, Error>,
                 file: StaticString = #filePath,
                 line: UInt = #line) {
         expect(sut, toRetrieve: expectedResult, file: file, line: line)
@@ -124,7 +124,7 @@ extension FeedStoreSpecs where Self: XCTestCase {
     }
     
     func expect(_ sut: FeedStore,
-                toRetrieve expectedResult: FeedStore.RetrievalResult,
+                toRetrieve expectedResult: Result<CachedFeed?, Error>,
                 file: StaticString = #filePath,
                 line: UInt = #line) {
         let retrievedResult = Result { try sut.retrieve() }
