273c39b1ec3f80bfb74c035fad9ed291540c358c
Jess Phillips <tripphillips5@gmail.com>
2023-01-31 21:04:13 -0700

    task(InMemoryFeedStore): make in memory implementation sync

--- EssentialFeed/EssentialApp/EssentialAppTests/Helpers/InMemoryFeedStore.swift
+++ EssentialFeed/EssentialApp/EssentialAppTests/Helpers/InMemoryFeedStore.swift
@@ -8,37 +8,42 @@
 import Foundation
 import EssentialFeed
 
-class InMemoryFeedStore: FeedStore, FeedImageDataStore {
+class InMemoryFeedStore {
     private(set) var feedCache: CachedFeed?
     private var feedImageDataCache: [URL: Data] = [:]
-
-    init(feedCache: CachedFeed? = nil) {
+    
+    private init(feedCache: CachedFeed? = nil) {
         self.feedCache = feedCache
     }
-    
+}
+
+extension InMemoryFeedStore: FeedStore {
     func deleteCachedFeed(completion: @escaping FeedStore.DeletionCompletion) {
         feedCache = nil
         completion(.success(()))
     }
-
+    
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
         feedCache = CachedFeed(feed: feed, timestamp: timestamp)
         completion(.success(()))
     }
-
+    
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
         completion(.success(feedCache))
     }
+}
 
-    func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+extension InMemoryFeedStore: FeedImageDataStore {
+    func insert(_ data: Data, for url: URL) throws {
         feedImageDataCache[url] = data
-        completion(.success(()))
     }
-
-    func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
-        completion(.success(feedImageDataCache[url]))
+    
+    func retrieve(dataForURL url: URL) throws -> Data? {
+        feedImageDataCache[url]
     }
+}
 
+extension InMemoryFeedStore {
     static var empty: InMemoryFeedStore {
         InMemoryFeedStore()
     }
